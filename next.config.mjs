c1194eab622775d298978463ad8a838de48d7b81
farjuu333 <farjanapinky350@gmail.com>
2026-04-29 23:30:55 +0600

    added top generations

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        
+      },
+    ],
+  },
+  // reactCompiler: true,
 };
 
 export default nextConfig;
